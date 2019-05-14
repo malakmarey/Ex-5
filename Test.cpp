79fe1d4f70c9b0f62802b8a261187d7f55c840aa
malakmarey <45039969+malakmarey@users.noreply.github.com>
2019-05-15 02:21:23 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,156 +1,293 @@
+ /**
+ * A demo program for itertools.
+ *
+ * @author Erel Segal-Halevi
+ * @since  2019-05
+ */
+
+
 #include <iostream>
-#include <string>
-#include "chain.hpp"
-#include "powerset.hpp"
-#include "product.hpp"
-#include "range.hpp"
-#include "zip.hpp"
-#include "badkan.hpp"
 using namespace std;
+
+#include "range.hpp"
+#include "chain.hpp"
+#include "zip.hpp"
+#include "product.hpp"
+#include "powerset.hpp"
+#include "badkan.hpp"
+#define COMMA ,
+
 using namespace itertools;
 
-int main()
+
+int main() {
+
+	badkan::TestCase testcase;
+	int grade=0;
+	int signal = setjmp(badkan::longjmp_buffer);
+	if (signal == 0) {
+
+
+
+//		Constant Range,Chain,Zip,Product,Powerset
+
+
+		range a11{1, 1};	range a044{0, 44};range Saz{'a', 'z'};range Saa{'a', 'a'};range Smq{'m' , 'q'};
+		
+		chain CR14R03{range{1, 4}, range{0, 3}}; chain CRACRDG{range{'a', 'c'}, range{'d', 'g'}};chain CACSTEST{range{'a', 'c'}, string{"test"}};
+		
+		zip ZCR14RAD{range{1, 4}, range{'a', 'd'}}; 	zip ZCACSAB{range{'a', 'c'}, string{"ab"}};
+
+		product PR14RAD{range{1, 4}, range{'a', 'd'}}; product PCACSAB{range{'a', 'c'}, string{"hello"}};
+	
+
+
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////// TEST RANGE 	/////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//	Simple tests or we only advance the figure is we check if it is equal.
+	
+		testcase.setname("TEST RANGE ");
+	
+		int number = 0;
+			for (int i : range(0, 9))
+			{
+				testcase.CHECK_EQUAL(i, number);
+				number++;
+			}
+		number =10;
+			for (int i : range(10, 44))
+			{
+				testcase.CHECK_EQUAL(i, number);
+				number++;
+			}
+		 number = -8;
+			for (int i : range(-8, 10))
+			{
+				testcase.CHECK_EQUAL(i,number);
+				number++;
+			}
+
+	char chartemp='a';
+			for (char i : range('a', 'd'))
+			{
+				testcase.CHECK_EQUAL(i, chartemp);
+			chartemp++;
+			}
+
+
+			chartemp='e';
+			for (char i : range('e', 'z'))
+			{
+				testcase.CHECK_EQUAL(i, chartemp);
+			chartemp++;
+			}
+			for (int i : range(0, 0))
+				testcase.CHECK_EQUAL(i, 0);
+
+
+			for (int i : a11)
+				testcase.CHECK_OUTPUT(i,"1");
+
+
+
+
+
+
+
+
+
+/*From there we will find tests that we walk in the following way.
+We find a table that is in fact the data that must be received each time.
+And simply, it remains more than check the outgoing data with what we expect to return
+*/
+
+
+
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////// TEST CHAIN 	/////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+
+
+testcase.setname("TEST CHAIN ");
+
+number=0;
+int tab[]={1,2,3,5,6,7};
+for (int i: chain(range(1,4), range(5,8)))
+	{
+		testcase.CHECK_EQUAL(i, tab[number]);
+		number++;
+	}
+
+
+number=0;
+int tab2[]={0,1,2,3,9,10};
+for (int i: chain(range(0,4), range(9,11)))
+	{
+		testcase.CHECK_EQUAL(i, tab2[number]);
+		number++;
+	}
+
+number=0;
+int temp=0;
+char tab3[]={'h','e','l','l','o'};
+char tab4[]={'a','b','c'}	;
+
+for (auto i: chain(range('a','d'), string("hello")))
+	{
+		if(number<3)
+		{
+		testcase.CHECK_EQUAL(i, tab4[number]);
+		number++;
+		}
+	else{
+		testcase.CHECK_EQUAL(i, tab3[temp]);
+		temp++;
+	}
+	}
+
+
+	
+number=0;
+temp=0;
+char tab5[]={'d','e','f'};
+char tab6[]={'a','b','c'}	;
+
+for (auto i: chain(range('a','d'), range('d','g')))
+	{
+		if(number<3)
+		{
+		testcase.CHECK_EQUAL(i, tab6[number]);
+		number++;
+		}
+	else{
+		testcase.CHECK_EQUAL(i, tab5[temp]);
+		temp++;
+	}
+	}
+
+
+
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////// TEST ZIP 	/////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+		testcase.setname("TEST ZIP ");
+number=0;
+
+string tab9[]={"1,h","2,e","3,l","4,l","5,o"};
+for (auto pair: zip(range(1,6), string("hello")))
 {
+		testcase.CHECK_OUTPUT(pair,tab9[number]);
+		number++;
 
-    // SOME TESTS WERENT ADDED BECAUSE THE IMPLEMENTATION IS NEEDED
-    badkan::TestCase testcase;
-    int grade = 0;
-    int signal = setjmp(badkan::longjmp_buffer);
-    if (signal == 0)
-    {
-        string test1 = "";
-        string test2 = "";
-        string test3 = "";
-        string test4 = "";
-
-        // test = iterable_to_string(range(5, 9));
-        // test = iterable_to_string(range(5.1, 9.1));
-        // test = iterable_to_string(range('a', 'e'));
-        for (int i : range(5, 9))
-        {
-            test1.append(to_string(i));
-        }
-        for (int i : range(0, 10))
-        {
-            test2.append(to_string(i));
-        }
-        for (char i : range('a', 'e'))
-        {
-            test3 += i;
-        }
-        for (char i : range('a', 'z'))
-        {
-            test4 += i;
-        }
-        testcase.setname("Range")
-            .CHECK_OUTPUT(test1, "5678")                       // 5678
-            .CHECK_OUTPUT(test2, "0123456789")                 // 0123456789
-            .CHECK_OUTPUT(test3, "abcd")                       // abcd
-            .CHECK_OUTPUT(test4, "abcdefghijklmnopqrstuvwxy"); // abcdefghijklmnopqrstuvwxy
-
-        test1 = "";
-        test2 = "";
-        test3 = "";
-        test4 = "";
-
-        for (int i : chain(range(1, 4), range(5, 8)))
-        {
-            test1.append(to_string(i));
-        }
-        for (double i : chain(range(1.5, 4.5), range(5.5, 8.5)))
-        {
-            test2.append(to_string(i) + " ");
-        }
-        for (char i : chain(range('a', 'e'), string("hello")))
-        {
-            test3 += i;
-        }
-        for (char i : chain(range('a', 'z'), range('z', 'a')))
-        {
-            test4 += i;
-        }
-        testcase.setname("Chain")
-            .CHECK_OUTPUT(test1, "123567")                                                 // 123567
-            .CHECK_OUTPUT(test2, "1.500000 2.500000 3.500000 5.500000 6.500000 7.500000 ") // 1.500000 2.500000 3.500000 5.500000 6.500000 7.500000
-            .CHECK_OUTPUT(test3, "abcdhello")                                              // abcdhello
-            .CHECK_OUTPUT(test4, "abcdefghijklmnopqrstuvwxyyxwvutsrqponmlkjihgfedcba");    // abcdefghijklmnopqrstuvwxyyxwvutsrqponmlkjihgfedcba
-
-        test1 = "";
-        test2 = "";
-        test3 = "";
-        test4 = "";
-
-        for (auto pair : zip(range(1, 6), string("world")))
-        {
-            test1.append(to_string(pair) + " ");
-        }
-        for (auto pair : zip(zip(range(1, 4), string("xyz")), zip(string("abc"), range(6, 9))))
-        {
-            test2.append(to_string(pair) + " ");
-        }
-        for (auto pair : zip(range(1, 3), string("ac")))
-        {
-            test3.append(to_string(pair) + " ");
-        }
-        testcase.setname("Zip")
-            .CHECK_OUTPUT(test1, "1,w  2,o  3,r  4,l  5,d ")
-            .CHECK_OUTPUT(test2, "1,x,a,6  2,y,b,7  3,z,c,8 ")
-            .CHECK_OUTPUT(test3, "1,a 2,b ");
-
-        test1 = "";
-        test2 = "";
-        test3 = "";
-        test4 = "";
-
-        for (auto pair : product(range(1, 4), string("hello")))
-        {
-            test1.append(to_string(pair) + " ");
-        }
-        for (auto pair : product(range(1, 4), string("ad")))
-        {
-            test2.append(to_string(pair) + " ");
-        }
-        for (auto pair : product(range(1, 3), range(1, 3)))
-        {
-            test3.append(to_string(pair) + " ");
-        }
-        testcase.setname("Product")
-            .CHECK_OUTPUT(test1, "1,h  1,e  1,l  1,l  1,o  2,h  2,e  2,l  2,l  2,o  3,h  3,e  3,l  3,l  3,o ")
-            .CHECK_OUTPUT(test2, "1,a 1,d 2,a 2,d 3,a 3,d ")
-            .CHECK_OUTPUT(test3, "1,1 1,2 1,3 2,1 2,2 2,3 3,1 3,2 3,3 ");
-
-        test1 = "";
-        test2 = "";
-        test3 = "";
-        test4 = "";
-
-        for (auto subset : powerset(range(1, 4)))
-        {
-            test1.append(to_string(subset) + " ");
-        }
-        for (auto subset : powerset(chain(range('a', 'c'), range('x', 'z'))))
-        {
-            test2.append(to_string(subset) + " ");
-        }
-        testcase.setname("Powerset")
-            .CHECK_OUTPUT(test1, "{}{1}{2}{1,2}{3}{1,3}{2,3}{1,2,3} ")
-            .CHECK_OUTPUT(test2, "{}{a}{b}{a,b}{x}{a,x}{b,x}{a,b,x}{y}{a,y}{b,y}{a,b,y}{x,y}{a,x,y}{b,x,y}{a,b,x,y} ");
-
-        grade = testcase.grade();
-    }
-    else
-    {
-        testcase.print_signal(signal);
-        grade = 0;
-    }
-    cout << "Your grade is: " << grade << endl;
-    return 0;
 }
-template <typename Iterable>
-string iterable_to_string(const Iterable &iterable)
+
+number=0;
+		string tab10[]={"6,q","7,w","8,e","9,r","10,t"};
+for (auto pair: zip(range(6,11), string("qwert")))
 {
-    ostringstream ostr;
-    for (decltype(*iterable.begin()) i : iterable)
-        ostr << i << ",";
-    return ostr.str();
+		testcase.CHECK_OUTPUT(pair,tab10[number]);
+		number++;
+
+}
+
+number=0;
+		string tab11[]={"6,0","7,1","8,2","9,3","10,4"};
+for (auto pair: zip(range(6,11), range(0,5)))
+{
+		testcase.CHECK_OUTPUT(pair,tab11[number]);
+		number++;
+
+}
+
+number=0;
+		string tab16[]={"6,a","7,b","8,c","9,d","10,e"};
+for (auto pair: zip(range(6,11), range('a','f')))
+{
+		testcase.CHECK_OUTPUT(pair,tab16[number]);
+		number++;
+
+}
+
+number=0;
+		string tab12[]={"0,a","1,b","2,c","3,d","4,e","5,f"};
+for (auto pair: zip(range(0,6), range('a','g')))
+{
+		testcase.CHECK_OUTPUT(pair,tab12[number]);
+		number++;
+
+}
+
+
+
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////// TEST PRODUCT 	/////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	testcase.setname(" TEST PRODUCT");
+	number=0;
+		string tab13[]={"0,a","0,b","1,a","1,b","2,a","2,b","3,a","3,b","4,a","4,b"};
+for (auto pair: product(range(0,5), string("ab")))
+{
+		testcase.CHECK_OUTPUT(pair,tab13[number]);
+		number++;
+
+}
+
+	number=0;
+		string tab14[]={"0,5","0,6","1,5","1,6","2,5","2,6","3,5","3,6","4,5","4,6"};
+for (auto pair: product(range(0,5), range(5,7)))
+{
+		testcase.CHECK_OUTPUT(pair,tab14[number]);
+		number++;
+
+}
+
+
+	number=0;
+		string tab17[]={"0,a","0,b","1,a","1,b","2,a","2,b","3,a","3,b","4,a","4,b"};
+for (auto pair: product(range(0,5), range('a','c')))
+{
+		testcase.CHECK_OUTPUT(pair,tab17[number]);
+		number++;
+
+}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+		////////////////////////////////// TEST PowerSet 	/////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////
+	testcase.setname(" TEST PowerSet");
+	number=0;
+		string tab18[]={"{}","{1}","{2}","{1,2}","{3}","{1,3}","{2,3}","{1,2,3}"};
+for (auto pair: powerset(range(1,4))){
+		testcase.CHECK_OUTPUT(pair,tab18[number]);
+		number++;
+
+}
+
+	number=0;
+		string tab20[]={"{}","{a}","{b}","{a,b}","{x}","{a,x}","{b,x}","{a,b,x}","{y}","{a,y}","{b,y}","{a,b,y}","{x,y}","{a,x,y}","{b,x,y}","{a,b,x,y}"};
+for (auto subset: powerset(chain(range('a','c'),range('x','z'))))
+{		testcase.CHECK_OUTPUT(subset,tab20[number]);
+		number++;
+
+}
+
+    testcase.print(cout, /*show_grade=*/false);
+    grade = testcase.grade();
+	} else {
+		testcase.print_signal(signal);
+		grade = 0;
+	}
+	cout << "Your grade is: "  << grade << endl;
+	return 0;
 }
