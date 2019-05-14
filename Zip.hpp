ef2dbc4a3f890999701cc64b8874b2f3adf3d443
malakmarey <45039969+malakmarey@users.noreply.github.com>
2019-05-15 02:23:03 +0300

    Update Zip.hpp

--- Zip.hpp
+++ Zip.hpp
@@ -1,94 +1,62 @@
-#pragma once
-/* 
-Resources:
-https://stackoverflow.com/questions/19923353/multiple-typename-arguments-in-c-template
-http://www.ocoudert.com/blog/2010/07/07/how-to-write-abstract-iterators-in-c/
-*/
-namespace itertools
-{
+#include "iostream"
 
-template <typename T1, typename T2>
-class zip_class
-{
-
-private:
-    T1 start_1;
-    T2 start_2;
-
-public:
-    zip_class(T1 s1, T2 s2) : start_1(s1), start_2(s2)
-    {
-    }
-
-    template <typename A, typename B>
-    class Itr
-    {
-    private:
-        A s1;
-        B s2;
-
-        bool is_A_fin() const
-        {
-            if (s1 == s1.end())
-            {
-                return false;
-            }
-            return true;
-        }
+//NameSpace for a Tasks
+namespace itertools {
+    
+    template <typename T1, typename T2> 
+    class zip {
+    
+    private: // private variables and functions
+        T1 iterable_A;
+        T2 iterable_B;
 
     public:
-        Itr(A _s1, B _s2) : s1(_s1), s2(_s2)
-        {
-        }
-        // Dereference
-        auto &operator*() const
-        {
-            if (is_A_fin())
-            {
-                return *s2;
-            }
-            return *s1;
-        }
-        // ++i
-        Itr<A, B> &operator++()
-        {
-            if (is_A_fin())
-            {
-                s2++;
-            }
-            else
-            {
-                s1++;
-            }
-
-            return *this;
-        } 
-          // Not-Equal comparison
-        bool operator!=(const Itr<A, B> &rhs) const
-        {
-            if (is_A_fin())
-            {
-                return s1 != rhs.s1;
-            }
-            return s2 != rhs.s2;
-        }
+        zip(T1 start, T2 end) :  iterable_A(start), iterable_B(end) {}
         
-     
+    auto end() {
+        return iterator<decltype(iterable_A.end()),decltype(iterable_B.end())>(iterable_A.end(), iterable_B.end()); }  // iteratable object  
+ auto begin(){ 
+        return  iterator<decltype(iterable_A.begin()),decltype(iterable_B.begin())>(iterable_A.begin(), iterable_B.begin()); }  // iteratable object
+
+    template <typename C1, typename C2>
+        class iterator {
+
+        private: 
+            C2 iter_B; // iterator B
+            C1 iter_A; // iterator A
+
+        public:
+            iterator(C1 itA , C2 itB): iter_A(itA) , iter_B(itB)  {}
+
+           
+ bool operator!=(iterator<C1,C2>  it){
+                return (iter_A != it.iter_A) && (iter_B != it.iter_B);
+ 
+            }
+
+            std::pair<decltype(*iter_A),decltype(*iter_B)> operator*() const {
+
+             return  std::pair<decltype(*iter_A),decltype(*iter_B)> (*iter_A , *iter_B);
+            }
+
+           
+            iterator<C1,C2>& operator++() {
+               ++iter_A;
+               ++iter_B;
+               return *this;
+ 
+                            }
+
+
+         
+        }; // END OF CLASS ITERATOR
+
+
     };
+    template <typename T,typename E>
+    std::ostream &operator<<(std::ostream &os, const std::pair<T,E> &c){
+    os << c.first << ',' << c.second;
 
-    auto begin() const
-    {
-        return (start_1.begin(), start_2.begin());
-    }
-
-    auto end() const
-    {
-        return (start_1.end(), start_2.end());
-    }
-};
-template <typename T1, typename T2>
-zip_class<T1, T2> zip(T1 start1, T2 start2)
-{
-    return zip_class<T1, T2>(start1, start2);
+    return os;
 }
-} // namespace itertools
+}
